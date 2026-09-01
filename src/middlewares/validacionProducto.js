2e436784982ebaaaac41201f65e434923776e723
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-01 19:35:16 -0300

    Valide las propiedades del precio y la descripcion del producto

--- src/middlewares/validacionProducto.js
+++ src/middlewares/validacionProducto.js
@@ -4,6 +4,17 @@ import resultadoValidacion from "./resultadoValidacion.js";
 export const validacionProducto = [
     body("nombreProducto")
         .notEmpty().withMessage("El nombre del producto es un dato obligatorio")
-        .isLength({min: 3, max: 50}).withMessage("El nombre del producto debe tener entre 3 y 50 caracteres")
-        .isString().withMessage("El nombre del producto debe ser un texto"),
+        .isString().withMessage("El nombre del producto debe ser un texto")
+        .isLength({min: 3, max: 50}).withMessage("El nombre del producto debe tener entre 3 y 50 caracteres"),
+        
+    body("precio")
+        .notEmpty().withMessage("El precio es un dato obligatorio")
+        .isNumeric().withMessage("El precio debe ser en formato numérico"),
+
+    body("descripcion")
+        .notEmpty().withMessage("La descripción es un dato obligatorio")
+        .isString().withMessage("La descripción debe ser un texto")
+        .isLength({min: 5, max: 100}).withMessage("La descripción debe tener entre 5 y 100 caracteres"),
+
+        resultadoValidacion
 ]
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { agregarProducto, listarProductos, buscarProductoPorID, borrarProductoPorID, editarProductoPorID} from "../controllers/productos.controllers.js";
+import { validacionProducto } from "../middlewares/validacionProducto.js";
 
 const router = Router();
 
-router.route("/").post(agregarProducto).get(listarProductos);
+router.route("/").post(validacionProducto, agregarProducto).get(listarProductos);
 router.route("/:id").get(buscarProductoPorID).delete(borrarProductoPorID).put(editarProductoPorID).patch(editarProductoPorID);
 
 export default router;
