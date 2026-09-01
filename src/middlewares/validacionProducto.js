2c348531942fff80c713d5ad45b8a63db52f4fa9
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-01 20:03:03 -0300

    Dividimos las reglas de validacion para el put y el post

--- src/middlewares/validacionProducto.js
+++ src/middlewares/validacionProducto.js
@@ -1,33 +1,28 @@
 import {body, param} from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 
-export const validacionProducto = [
+const reglasProducto = [
     body("nombreProducto")
-        .notEmpty().withMessage("El nombre del producto es un dato obligatorio")
         .isString().withMessage("El nombre del producto debe ser un texto")
         .isLength({min: 3, max: 50}).withMessage("El nombre del producto debe tener entre 3 y 50 caracteres"),
         
     body("precio")
-        .notEmpty().withMessage("El precio es un dato obligatorio")
         .isNumeric().withMessage("El precio debe ser en formato numérico"),
 
     body("descripcion")
-        .notEmpty().withMessage("La descripción es un dato obligatorio")
         .isString().withMessage("La descripción debe ser un texto")
         .isLength({min: 5, max: 100}).withMessage("La descripción debe tener entre 5 y 100 caracteres"),
 
     body("imagen")
-        .notEmpty().withMessage("La imagen es un dato obligatorio")
         .isString().withMessage("La imagen debe ser un texto")
         .matches(/^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/).withMessage("La imagen debe ser una URL válida que comience con https:// y termine con una extensión de imagen válida (jpg, jpeg, png, webp, avif, svg)"),
     
     body("categoria")
-        .notEmpty().withMessage("La categoría es un dato obligatorio")
         .isString().withMessage("La categoría debe ser un texto")
         .isIn(["Indumentaria deportiva", "Calzado", "Accesorios deportivos", "Pelotas", "Bebidas", "Snacks"]).withMessage("La categoría debe ser una de las opciones válidas"),
-        
-        resultadoValidacion
 ]
+
+export const validacionProducto = [...reglasProducto.map((regla) => regla.notEmpty().withMessage("Este campo es un datoobligatorio")), resultadoValidacion];
 
 export const validacionIDProducto = [
     param('id').isMongoId().withMessage("El ID enviado no tiene el formato de ID de MongoDB"), resultadoValidacion
