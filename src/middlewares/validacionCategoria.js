6a312eb010965bab77b78922237a1c5853a365de
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-02 19:36:59 -0300

    Agregue las validaciones para mi modelo categoria

---
+++ src/middlewares/validacionCategoria.js
@@ -0,0 +1,22 @@
+import { body, param } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+
+export const validacionCategoria = [
+    body("nombreCategoria")
+        .notEmpty()
+        .withMessage("El nombre de la categoría es un dato obligatorio")
+        .isString()
+        .withMessage("El nombre de la categoría debe ser un texto")
+        .isLength({ min: 5, max: 100 })
+        .withMessage("El nombre de la categoría debe tener entre 5 y 100 caracteres"),
+
+    resultadoValidacion
+];
+
+export const validacionIDCategoria = [
+    param("id")
+        .isMongoId()
+        .withMessage("El ID enviado no tiene el formato de ID de MongoDB"),
+
+    resultadoValidacion
+];
