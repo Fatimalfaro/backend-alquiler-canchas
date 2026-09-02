9d9d92a789aefe0125ec454279163e852bf278c3
Abel Almaraz <almarazabeljesus@gmail.com>
2026-09-02 02:08:58 -0300

    feat: integrar rutas de usuarios

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,11 @@
-import {Router} from "express";
+import { Router } from "express";
+
 import productos from "./productos.routes.js";
+import usuarios from "./usuario.routes.js";
+
 const router = Router();
 
 router.use("/producto", productos);
+router.use("/usuario", usuarios);
 
 export default router;
--- src/server/config.js
+++ src/server/config.js
@@ -4,18 +4,22 @@ import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import '../database/db.js'
+import rutas from "../routes/index.routes.js";
 
 export default class Server {
   constructor() {
     this.app = express();
     this.PORT = process.env.PORT || 3000;
     this.middlewares();
+    
   }
 
   middlewares() {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(morgan("dev"))
+    this.app.use("/api", rutas);
+
     const __dirname = dirname(fileURLToPath(import.meta.url));
     this.app.use(express.static(__dirname + "/../../public"));
     
