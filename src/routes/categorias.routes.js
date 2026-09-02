71ed6229268a6eaae75eb85e5b80a69679ec8750
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-02 19:06:13 -0300

    Agregue las rutas y el controladores para poder listar todas las categorias, buscar por id, borrar por id y editar por id una categoria

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { crearCategoria } from "../controllers/categorias.controllers.js";
-
+import { crearCategoria, listarCategorias, buscarCategoriaPorID, borrarCategoriaPorID, editarCategoriaPorID } from "../controllers/categorias.controllers.js";
 const router = Router();
 
-router.route('/').post(crearCategoria)
+router.route('/').post(crearCategoria).get(listarCategorias);
+router.route('/:id').get(buscarCategoriaPorID).delete(borrarCategoriaPorID).put(editarCategoriaPorID).patch(editarCategoriaPorID);
 
 export default router;
