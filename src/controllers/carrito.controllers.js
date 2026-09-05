5d87a3e9bc2add74e4044d27f4b1c47e7c38abf9
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-05 00:08:45 -0300

    Agregue el metodo populate para que no se devuelva solo el id del producto si no tambien su nombre precio e imagen

--- src/controllers/carrito.controllers.js
+++ src/controllers/carrito.controllers.js
@@ -1,17 +1,18 @@
 import Producto from "../models/producto.js";
+import buscarOcrearCarrito from "../utils/buscarOcrearCarrito.js";
 
 
 export const agregarAlCarrito = async (req, res) => {
     try {
        const { producto, cantidad } = req.body;
-       const usuarioId = req.user.id;
+       const usuarioId = req.usuario.id;
        
        const productoExistente = await Producto.findById(producto);
        if (!productoExistente) {
-           return res.status(404).json({ message: 'No se encontro un producto con el ID enviado' });
+           return res.status(404).json({ mensaje: 'No se encontro un producto con el ID enviado' });
        }
        
-       const carrito = buscarOcrearCarrito(usuarioId);
+       const carrito = await buscarOcrearCarrito(usuarioId);
 
        const itemIndex = carrito.items.findIndex((item) => item.producto.toString() === producto);
        
@@ -23,8 +24,11 @@ export const agregarAlCarrito = async (req, res) => {
             cantidad 
         });
        }
-       
+       await carrito.save();
+       await carrito.populate("items.producto", "nombreProducto precio imagen");
+       res.status(200).json({ mensaje: 'Producto agregado al carrito exitosamente', carrito });
+
     }catch(error){
-        res.status(500).json({ message: 'Ocurrio un error al intentar agregar un producto al carrito'});
+        res.status(500).json({ mensaje: 'Ocurrio un error al intentar agregar un producto al carrito'});
     }
 }
