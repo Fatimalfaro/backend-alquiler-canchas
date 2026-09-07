0fa8e0f1b3713e835b9a0e9d5cb07baa3d49aa4f
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-07 16:57:20 -0300

    Para poder crear mi preferencia de pago, traje los datos del usuario, y me fijo si este usuario tiene un carrito si lo tiene recorro todos los items para encontrar que debe pagar

--- src/controllers/pago.controllers.js
+++ src/controllers/pago.controllers.js
@@ -1,11 +1,42 @@
 import {MercadoPagoConfig} from "mercadopago";
+import buscarOcrearCarrito from "../utils/buscarOcrearCarrito.js";
 
 const client = new MercadoPagoConfig({accessToken: process.env.MP_ACCESS_TOKEN});
 
-
-
 export const crearPreferenciaPago = async(req,res)=>{
     try{
+        const userId = req.user.id;
+        const carrito = await buscarOcrearCarrito(userId)
+        await carrito.populate('items.producto')
+
+        if(carrito.items.length === 0){
+            return res.status(400).json({mensaje: 'El carrito esta vacio'})
+        }
+
+        let montoTotal = 0
+
+        const itemsMP = carrito.items.map((item)=>{
+            const subTotal = item.producto.precio * item.cantidad;
+            montoTotal += subTotal;
+
+            return{
+                id: item.producto._id.toString(),
+                title: item.producto.nombreProducto,
+                unit_price: Number(item.producto.precio),
+                quality: Number(item.cantidad),
+                currency_id: "ARS",
+                picture_url: item.producto.imagen
+            }
+        }
+
+    
+    )
+
+
+
+
+
+        res.status(201).json(carrito)
 
     }catch(error){
         console.error(error);
--- src/routes/pago.routes.js
+++ src/routes/pago.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
-import { crearPreferenciaPago } from "../controllers/pago.controllers";
-import {verificarToken} from "../middlewares/authMiddleware"
+import { crearPreferenciaPago } from "../controllers/pago.controllers.js";
+import {verificarToken} from "../middlewares/authMiddleware.js"
 const router = Router();
 
 router.route("/crear-preferencia").post(verificarToken, crearPreferenciaPago)
