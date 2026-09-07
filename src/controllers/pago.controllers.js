1b697fb66a66d803123d7999ced97a1489984247
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-07 17:20:44 -0300

    Agregue código en mi controlador para crear la orden y la guarde en la base de datos

--- src/controllers/pago.controllers.js
+++ src/controllers/pago.controllers.js
@@ -1,5 +1,6 @@
 import {MercadoPagoConfig} from "mercadopago";
 import buscarOcrearCarrito from "../utils/buscarOcrearCarrito.js";
+import Orden from "../models/orden.js";
 
 const client = new MercadoPagoConfig({accessToken: process.env.MP_ACCESS_TOKEN});
 
@@ -23,17 +24,28 @@ export const crearPreferenciaPago = async(req,res)=>{
                 id: item.producto._id.toString(),
                 title: item.producto.nombreProducto,
                 unit_price: Number(item.producto.precio),
-                quality: Number(item.cantidad),
+                quantity: Number(item.cantidad),
                 currency_id: "ARS",
                 picture_url: item.producto.imagen
-            }
+            };
         }
+    );
 
-    
-    )
+            const itemsOrden = carrito.items.map((item)=>({
+                producto: item.servicio._id,
+                nombreProducto: item.producto.nombreProducto,
+                precioUnitario: item.producto.precio,
+                cantidad: item.cantidad
+            }));
 
+            const nuevaOrden = new Orden({
+                usuario: userId,
+                items: itemsOrden,
+                montoTotal,
+                estado: 'pendiente'
+            })
 
-
+            await nuevaOrden.save()
 
 
         res.status(201).json(carrito)
