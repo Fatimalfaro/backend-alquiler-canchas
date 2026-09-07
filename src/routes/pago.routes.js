46b153ba08195d8c4379b3f32c9e6cb423064b73
Fatimalfaro <127248459+Fatimalfaro@users.noreply.github.com>
2026-09-07 20:28:25 -0300

    Revert "Feature/mercado pago"

--- src/routes/pago.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import { crearPreferenciaPago } from "../controllers/pago.controllers.js";
-import {verificarToken} from "../middlewares/authMiddleware.js"
-const router = Router();
-
-router.route("/crear-preferencia").post(verificarToken, crearPreferenciaPago)
-
-export default router;
