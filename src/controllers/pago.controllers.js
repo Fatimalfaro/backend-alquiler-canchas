46fb2ddc0db8175b76d4d9c3d901f7f6ff71fc49
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-08 20:09:49 -0300

    Agregue y configure ngrok

--- src/controllers/pago.controllers.js
+++ src/controllers/pago.controllers.js
@@ -1,4 +1,4 @@
-import {MercadoPagoConfig, Preference} from "mercadopago";
+import {MercadoPagoConfig, Preference, Payment} from "mercadopago";
 import buscarOcrearCarrito from "../utils/buscarOcrearCarrito.js";
 import Orden from "../models/orden.js";
 
