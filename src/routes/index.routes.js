3356c5a94796837eb48e69764b8da434f9955dca
Abel Almaraz <almarazabeljesus@gmail.com>
2026-09-09 02:14:20 -0300

    feat: implementa CRUD de reservas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,8 +3,9 @@ import productos from "./productos.routes.js";
 import usuarios from "./usuario.routes.js";
 import categorias from "./categorias.routes.js";
 import carrito from "./carrito.routes.js";
-import pagoRouter from "./pago.routes.js"
+import pagoRouter from "./pago.routes.js";
 import canchas from "./canchas.routes.js";
+import reservas from "./reservas.routes.js";
 
 const router = Router();
 
@@ -12,7 +13,8 @@ router.use("/producto", productos);
 router.use("/usuario", usuarios);
 router.use("/categorias", categorias);
 router.use("/carrito", carrito);
-router.use('/pago', pagoRouter)
+router.use("/pago", pagoRouter);
 router.use("/canchas", canchas);
+router.use("/reservas", reservas);
 
 export default router;
