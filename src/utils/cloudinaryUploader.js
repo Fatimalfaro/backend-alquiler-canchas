2171e9a7fc466a2469e22ef1f8507dc01b84afcf
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-09 00:12:37 -0300

    Agregue código que me permite cumplir o no una promesa de subir imagenes a una carpeta en cloudinary

---
+++ src/utils/cloudinaryUploader.js
@@ -0,0 +1,19 @@
+import cloudinary from "./cloudinary";
+
+/**
+ * @param {Buffer} buffer
+ * @returns {Promise<Object>}
+ */
+
+const subirImagenACloudinary = (buffer) =>{
+    return new Promise ((resolve, reject)=>{
+        const stream = cloudinary.uploader.upload_stream({folder: 'imagenes-cancha'}, (error, result)=>{
+            if(result){
+                resolve(result)
+            }else{
+                reject(error)
+            }
+        })
+        stream.end(buffer)
+    })
+}
