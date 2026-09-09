a60aaed653d390f0a79a4722fa307d1671f3c4f4
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-09 00:23:31 -0300

    Agregue un middleware para manejar el error de exceso de tamaño de una imagen

---
+++ src/middlewares/errorMulter.js
@@ -0,0 +1,8 @@
+const errorMulter = (err, _req, res, next) => {
+    if(err && err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(400).json({mensaje: 'Ña imagen no puede superar los 2MB'})
+    }
+    next(err)
+}
+
+export default errorMulter;
