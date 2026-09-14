0c5eecab990c75c5af60fea63a8e4b5004ed2876
Abel Almaraz <almarazabeljesus@gmail.com>
2026-09-13 21:55:38 -0300

    feat: agregar endpoint de mis compras

--- src/routes/pago.routes.js
+++ src/routes/pago.routes.js
@@ -4,6 +4,7 @@ import {
   crearPreferenciaPago,
   crearPreferenciaPagoReserva,
   recibirWebhook,
+  obtenerMisCompras,
 } from "../controllers/pago.controllers.js";
 
 import { verificarToken } from "../middlewares/authMiddleware.js";
@@ -15,6 +16,8 @@ router.route("/crear-preferencia").post(verificarToken, crearPreferenciaPago);
 router
   .route("/crear-preferencia-reserva")
   .post(verificarToken, crearPreferenciaPagoReserva);
+
+router.route("/mis-compras").get(verificarToken, obtenerMisCompras);
 
 router.route("/webhook").get(recibirWebhook).post(recibirWebhook);
 
