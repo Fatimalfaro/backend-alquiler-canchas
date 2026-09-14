c53cf207a4c27f2ed307eefe430eabf215f5b81d
Fátima Alfaro <fatialfaro99@gmail.com>
2026-09-14 10:34:39 -0300

    Elimine ifs qu no permitian que el usuario admin tuviera acceso a todas las funcionalidades

--- src/middlewares/validacionCategoria.js
+++ src/middlewares/validacionCategoria.js
@@ -1,30 +1,38 @@
 import { body, param } from "express-validator";
-import resultadoValidacion from "./resultadoValidacion.js";
+
+import { resultadoValidacion } from "./resultadoValidacion.js";
 
 export const validacionCategoria = [
-    body("nombreCategoria")
-        .notEmpty()
-        .withMessage("El nombre de la categoría es un dato obligatorio")
-        .isString()
-        .withMessage("El nombre de la categoría debe ser un texto")
-        .isLength({ min: 5, max: 100 })
-        .withMessage("El nombre de la categoría debe tener entre 5 y 100 caracteres"),
+  body("nombreCategoria")
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre de la categoría es obligatorio")
+    .isLength({ min: 5, max: 100 })
+    .withMessage("El nombre debe tener entre 5 y 100 caracteres")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$/)
+    .withMessage("El nombre solo puede contener letras y espacios"),
 
-    resultadoValidacion
+  resultadoValidacion,
 ];
 
 export const validacionIDCategoria = [
-    param("id")
-        .isMongoId()
-        .withMessage("El ID enviado no tiene el formato de ID de MongoDB")
-        .bail()
-        .custom(async (id) => {
-            const categoriaExiste = await Categoria.findById(id);
-            if (!categoriaExiste) {
-                throw new Error("La categoria seleccionada no existe");
-            }
-            return true;
-        }),
+  param("id")
+    .isMongoId()
+    .withMessage("El ID de la categoría no es válido"),
 
-    resultadoValidacion
+  resultadoValidacion,
+];
+
+export const validacionPatchCategoria = [
+  body("nombreCategoria")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre de la categoría no puede estar vacío")
+    .isLength({ min: 5, max: 100 })
+    .withMessage("El nombre debe tener entre 5 y 100 caracteres")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$/)
+    .withMessage("El nombre solo puede contener letras y espacios"),
+
+  resultadoValidacion,
 ];
